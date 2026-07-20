363d9b4d94c9b67e0944defda1260092654c0841
Aashish Adhikari <aashish.adk156@gmail.com>
2026-07-20 22:52:28 +0545

    added bcrypt hashing and added user into the database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,15 +2,15 @@ import { registerUserService } from "../services/user.service.js";
 
 export const registerUser = async (req, res) => {
     try {
-        const { full_name, email, password, role } = req.body;
+        const { fullName, email, password, role } = req.body;
 
-        if (!full_name || !email || !password || !role) {
+        if (!fullName || !email || !password || !role) {
             return res.status(400).json({
                 success: false,
                 message: "All required fields are mandatory"
             })
         }
-        const user = await registerUserService({ full_name, email, password, role });
+        const user = await registerUserService({ fullName, email, password, role });
         res.status(201).json({
             success: true,
             message: "User registered successfully",
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,21 @@
 import { pool } from '../config/db.js';
-export const registerUserService = async ({ full_name, email, password, role }) => {
-    const queryText = 'SELECT 1 FROM users WHERE email = $1 LIMIT 1';
-    const queryResult = await pool.query(queryText, [email.toLowerCase().trim()]);
+import bcrypt from 'bcrypt';
+import { hashPassword } from '../utils/hashPassword.js';
+
+export const registerUserService = async ({ fullName, email, password, role }) => {
+    const normalizedEmail = email.toLowerCase().trim();
+    const existingEmailQueryText = 'SELECT 1 FROM users WHERE email = $1 LIMIT 1';
+    const queryResult = await pool.query(existingEmailQueryText, [normalizedEmail]);
     if (queryResult.rows.length > 0) {
         const error = new Error("User with this email already exists");
         error.statusCode = 409;
         throw error;
     }
+    
+    const hashedPassword = await hashPassword(password);
+    const insertUserQueryText = 'INSERT INTO users (full_name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING id,full_name,email,role,created_at,updated_at';
+    const newUser = await pool.query(insertUserQueryText, [fullName, normalizedEmail, hashedPassword, role]);
+
+    return newUser.rows[0];
+
 };
